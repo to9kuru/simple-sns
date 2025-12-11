73ce4ae5ca7127391163f3d0eff183c82d029530
kuR <150643796+to9kuru@users.noreply.github.com>
2025-12-11 19:36:11 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,17 @@
 export const metadata = {
-  title: "Simple SNS",
+title: "Simple SNS",
 };
 
+
 export default function RootLayout({ children }) {
-  return (
-    <html lang="ja">
-      <body>{children}</body>
-    </html>
-  );
+return (
+<html lang="ja">
+<body>
+<div className="top-menu">
+<a href="/ranking">⭐ ランキング</a>
+</div>
+{children}
+</body>
+</html>
+);
 }
